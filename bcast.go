0961fee1bc1b6e726873b09b5829595a6f05a7b0
Alexander I.Grafov <grafov@gmail.com>
2013-09-03 22:54:13 +0400

    Initial version. With tests.

---
+++ bcast.go
@@ -0,0 +1,104 @@
+// Broadcast over channels.
+package bcast
+
+/*
+   bcast package for Go. Broadcasting on a set of channels.
+   Copyright © 2013 Alexander I.Grafov <grafov@gmail.com>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see {http://www.gnu.org/licenses/}.
+*/
+
+import (
+	// 	h "github.com/emicklei/hopwatch"
+	"time"
+)
+
+type Message struct {
+	sender  *chan interface{}
+	payload interface{}
+}
+
+type Member struct {
+	group *Group            // send messages to others directly to group.In
+	In    *chan interface{} // get messages from others to own channel
+}
+
+type Group struct {
+	in  *chan Message       // receive broadcasts from members
+	out []*chan interface{} // broadcast messages to members
+}
+
+// Create new broadcast group.
+func NewGroup() *Group {
+	in := make(chan Message)
+	return &Group{in: &in}
+}
+
+// Broadcast messages received from one group member to others.
+// If not incoming messages during `timeout` then function returns. Set `timeout` to zero to skip timeout.
+func (r *Group) Broadcasting(timeout time.Duration) {
+	for {
+		select {
+		case received := <-*r.in:
+			switch received.payload.(type) {
+			case Member: // unjoining member
+
+				for i, addr := range r.out {
+					if addr == received.payload.(Member).In && received.sender == received.payload.(Member).In {
+						r.out = append(r.out[:i], r.out[i+1:]...)
+						break
+					}
+				}
+			default: // receive payload and broadcast it
+
+				for _, member := range r.out {
+					if *received.sender != *member { // not return broadcast to sender
+
+						go func(out *chan interface{}, received *Message) { // non blocking
+							*out <- received.payload
+						}(member, &received)
+
+					}
+				}
+			}
+		case <-time.After(timeout):
+			if timeout > 0 {
+				return
+			}
+		}
+	}
+}
+
+// Join new member to broadcast.
+func (r *Group) Join() *Member {
+	out := make(chan interface{})
+	r.out = append(r.out, &out)
+	return &Member{group: r, In: &out}
+}
+
+// Unjoin member from broadcast group.
+func (r *Member) Close() {
+	*r.group.in <- Message{sender: r.In, payload: *r} // broadcasting of self means member closing
+}
+
+// Broadcast Message to others.
+func (r *Member) Send(val interface{}) {
+	*r.group.in <- Message{sender: r.In, payload: val}
+}
+
+// Get broadcast Message.
+// As alternative you may get it from `In` channel.
+func (r *Member) Recv() interface{} {
+	return <-*r.In
+}
---
+++ bcast_test.go
@@ -0,0 +1,111 @@
+package bcast
+
+/*
+   bcast package for Go. Broadcasting on a set of channels.
+   Copyright © 2013 Alexander I.Grafov <grafov@gmail.com>
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see {http://www.gnu.org/licenses/}.
+*/
+
+import (
+	// h "github.com/emicklei/hopwatch"
+	"testing"
+	"time"
+)
+
+// Create new broadcast group.
+// Join two members.
+func TestNewGroupAndJoin(t *testing.T) {
+	group := NewGroup()
+	member1 := group.Join()
+	member2 := group.Join()
+	if member1.group != member2.group {
+		panic("group for these members must be same")
+	}
+}
+
+// Create new broadcast group.
+// Join two members.
+// Unjoin first member.
+func TestUnjoin(t *testing.T) {
+	group := NewGroup()
+	member1 := group.Join()
+	member2 := group.Join()
+	if len(group.out) != 2 {
+		panic("incorrect length of `out` slice")
+	}
+
+	go group.Broadcasting(0)
+
+	member1.Close()
+	time.Sleep(1 * time.Second) // because Broadcasting executed concurrently
+	if len(group.out) > 1 || group.out[0] != member2.In {
+		panic("unjoin member does not work")
+	}
+}
+
+// Create new broadcast group.
+// Join 12 members.
+// Broadcast one integer from each member.
+func TestBroadcast(t *testing.T) {
+	var valcount int
+
+	group := NewGroup()
+
+	for i := 1; i <= 12; i++ {
+		go func(i int, group *Group) {
+			m := group.Join()
+			m.Send(i)
+
+			for {
+				val := m.Recv()
+				if val.(int) == i {
+					panic("sent value was received by sender")
+				}
+				valcount++
+			}
+		}(i, group)
+	}
+
+	group.Broadcasting(100 * time.Millisecond)
+	if valcount != 12*12-12 { // number of channels * number of messages - number of channels
+		panic("not all messages broadcasted")
+	}
+}
+
+// Create new broadcast group.
+// Join 1024 members.
+// Broadcast one integer from each member.
+func TestBroadcastOnLargeNumberOfMembers(t *testing.T) {
+	var valcount int
+
+	group := NewGroup()
+	for i := 1; i <= 1024; i++ {
+		go func(i int, group *Group) {
+			m := group.Join()
+			m.Send(i)
+			for {
+				val := m.Recv()
+				if val.(int) == i {
+					panic("sent value was received by sender")
+				}
+				valcount++
+			}
+		}(i, group)
+	}
+	group.Broadcasting(100 * time.Millisecond)
+	if valcount != 1024*1024-1024 { // number of channels * number of messages - number of channels
+		panic("not all messages broadcasted")
+	}
+}
