c2f560e70ec08cced281cbc51d6b5c6680d25a36
Alexander I.Grafov <grafov@gmail.com>
2013-09-07 01:08:01 +0400

    Code relicensed under BSD 3-clause license.

--- bcast_test.go
+++ bcast_test.go
@@ -2,20 +2,11 @@ package bcast
 
 /*
    bcast package for Go. Broadcasting on a set of channels.
-   Copyright © 2013 Alexander I.Grafov <grafov@gmail.com>
 
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see {http://www.gnu.org/licenses/}.
+   Copyright © 2013 Alexander I.Grafov <grafov@gmail.com>.
+   All rights reserved.
+   Use of this source code is governed by a BSD-style
+   license that can be found in the LICENSE file.
 */
 
 import (
